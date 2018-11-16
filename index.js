3c6c01c33c7e2afbd48f930122a73fd873d2396a
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-11-16 19:47:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,4 +13,7 @@ newID = 1000000000 + oldID;
 ageIsValid =  Number.isInteger(currentAge);
 
  let randomNumber = Math.floor(Math.random()*20)+1;
+ 
+ let randomInteger = Math.round(randomNumber);
+ 
 
